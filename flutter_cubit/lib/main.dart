4797c3b781c4c31346724159310df1acfe8a89ca
Liqiuyue9597 <Liqiuyue9597@github.com>
2023-11-12 19:59:11 +0800

    第一个page

--- flutter_cubit/lib/main.dart
+++ flutter_cubit/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_cubit/cubit/app_cubit_logics.dart';
-import 'package:flutter_cubit/cubit/app_cubits.dart';
-import 'package:flutter_cubit/pages/detail_pages/cubit/store_page_info_cubits.dart';
-import 'package:flutter_cubit/pages/detail_pages/detail_page.dart';
-import 'package:flutter_cubit/pages/navpages/main_page.dart';
 import 'package:flutter_cubit/pages/welcome_page.dart';
-import 'package:flutter_cubit/services/data_services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,27 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            iconTheme: IconThemeData(
-              color:Colors.black
-            ),
-            elevation: 0,
-            backgroundColor: Colors.white
-          )
-        ),
-        home: MultiBlocProvider(
-          providers: [
-            BlocProvider<AppCubits>(
-              create: (context) => AppCubits(
-                data: DataServices(),
-              ),
-            ),
-            BlocProvider<StorePageInfoCubits>(
-              create: (context) => StorePageInfoCubits(),
-            )
-          ],
-          child: const AppCubitLogics(),
-        ));
+        theme: ThemeData(primaryColor: Colors.blue),
+        home: WelcomePage());
   }
 }
